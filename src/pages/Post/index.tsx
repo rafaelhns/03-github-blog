e176e82a414232079755691e32cd73ae9cf2b9d0
Rafael Honório <rafaelxhs@gmail.com>
2023-02-12 16:48:34 -0300

    Página Post

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,3 +1,49 @@
+import ReactMarkdown from 'react-markdown'
+import { NavLink } from 'react-router-dom'
+import { Icon } from '../../components/Icon'
+import {
+  BodyContainer,
+  NavLinks,
+  PostContainer,
+  PostData,
+  PostDataContainer,
+  Title,
+  TitleContainer,
+} from './styles'
+
 export function Post() {
-  return <div>--- POST ---</div>
+  return (
+    <PostContainer>
+      <TitleContainer>
+        <NavLinks>
+          <NavLink to="/">
+            <Icon variant="chevron-left" size={12} /> VOLTAR
+          </NavLink>
+          <NavLink to="/">
+            VER NO GITHUB <Icon variant="link" size={12} />
+          </NavLink>
+        </NavLinks>
+
+        <Title>Blog post title</Title>
+
+        <PostDataContainer>
+          <PostData>
+            <Icon variant="github" size={18} /> RafaelXau
+          </PostData>
+          <PostData>
+            <Icon variant="calendar" size={18} />
+            Postado em 10 de Junho de 2021
+          </PostData>
+          <PostData>
+            <Icon variant="comment" size={18} />
+            Postado em 10 de Junho de 2021
+          </PostData>
+        </PostDataContainer>
+      </TitleContainer>
+
+      <BodyContainer>
+        <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+      </BodyContainer>
+    </PostContainer>
+  )
 }
---
+++ src/pages/Post/styles.ts
@@ -0,0 +1,72 @@
+import styled from 'styled-components'
+
+export const PostContainer = styled.div`
+  max-width: ${(props) => props.theme['max-width']};
+  margin: -5.5rem auto 0;
+  padding: 0 1rem 2rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3rem;
+`
+
+export const TitleContainer = styled.div`
+  background-color: ${(props) => props.theme.colors['base-profile']};
+  width: 100%;
+  padding: 2rem 2.5rem;
+  border-radius: 10px;
+  box-shadow: 0px 2px 28px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  flex-direction: column;
+`
+
+export const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+
+  a {
+    color: ${(props) => props.theme.colors.blue};
+    font-size: 0.75rem;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+`
+
+export const Title = styled.h1`
+  font-size: 1.5rem;
+  color: ${(props) => props.theme.colors['base-title']};
+  margin: 1.25rem 0 0.5rem;
+`
+
+export const PostDataContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 1.5rem;
+`
+
+export const PostData = styled.span`
+  color: ${(props) => props.theme.colors['base-subtitle']};
+  text-decoration: none;
+  transition: color 0.2s;
+
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  img {
+    opacity: 0.4;
+  }
+`
+
+export const BodyContainer = styled.div`
+  width: 100%;
+  padding: 2.5rem 2rem;
+`
